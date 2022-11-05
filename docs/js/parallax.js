45390bb6f09c6d86f8e40f225f5506c65f867e6b
Folomieiev Maksym <Folomieiev.m@gmail.com>
2022-11-06 00:09:58 +0200

    up

--- docs/js/parallax.js
+++ docs/js/parallax.js
@@ -13,9 +13,9 @@ let currentTransform;
 console.log(parallaxElement);
 
 parallaxElement.forEach((el) => {
-   el.style.transform = "translateY(1px)";
+   el.style.transform = "translateY(0px)";
 });
-// parallax();
+parallax();
 function parallax(e) {
    parallaxParent.forEach((el) => {
       if (el.getBoundingClientRect().top <= viewportTop / 1) {
@@ -28,27 +28,30 @@ function parallax(e) {
             smallerElement = el.firstElementChild;
          }
          console.clear();
+         console.log(el.getBoundingClientRect().top);
          console.log("Прокрутка сверху = " + window.pageYOffset);
          // console.log("Высота вьюпорта = " + document.documentElement.clientHeight);
          parentHeight = el.getBoundingClientRect().height;
          console.log("Высота родителя = " + parentHeight);
          smallerElementHeight = smallerElement.getBoundingClientRect().height;
          console.log("Высота блока = " + smallerElementHeight);
-         smallerElementSpeed = parentHeight / smallerElementHeight;
          heightDifference = parentHeight - smallerElementHeight;
          console.log("Разница высот = " + heightDifference);
+         smallerElementSpeed = parentHeight / smallerElementHeight;
+         smallerElementSpeed = parentHeight / heightDifference;
+         // smallerElementSpeed = 1.1;
+         console.log("Скорость сдвига = " + smallerElementSpeed);
          tranfsorm = window.pageYOffset / smallerElementSpeed;
          currentTransform = parseInt(smallerElement.style.transform.match(/[-0-9.]+(?=px)/));
          console.log("Текущий трансформ = " + currentTransform);
-         // if (tranfsorm < parentHeight - smallerElementHeight) {
-         // }
-         // if (window.pageYOffset < parentHeight) {
+
+         // ! Done
          if (
-            (currentTransform < heightDifference && window.pageYOffset < heightDifference) ||
+            currentTransform < heightDifference ||
+            window.pageYOffset < heightDifference ||
             window.pageYOffset < currentTransform
          ) {
-            // if (heightDifference > currentTransform) {
-            smallerElement.style.transform = `translateY(${tranfsorm}px)`;
+            smallerElement.style.transform = `translateY(${tranfsorm - 3}px)`;
          }
       }
    });
