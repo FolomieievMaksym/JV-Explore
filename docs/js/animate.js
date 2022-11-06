d9a878260ded00438133a99c6c09dac9a13b22cd
Folomieiev Maksym <Folomieiev.m@gmail.com>
2022-11-06 20:53:12 +0200

    big update

--- docs/js/animate.js
+++ docs/js/animate.js
@@ -1,12 +1,38 @@
 let viewportHeight = document.documentElement.clientHeight;
 
 const itemsToAnimate = document.querySelectorAll("[data-animate]");
+const itemsToAnimateFromTop = document.querySelectorAll("[data-animate-from-top]");
+const itemsToAnimateFromBottom = document.querySelectorAll("[data-animate-from-bottom]");
+const itemsToAnimateFromLeft = document.querySelectorAll("[data-animate-from-left]");
+const itemsToAnimateFromRight = document.querySelectorAll("[data-animate-from-right]");
+const itemsToAnimateScale = document.querySelectorAll("[data-animate-scale]");
 
 // ? Базовые стили для анимируемых элементов
-itemsToAnimate.forEach((el) => {
-   el.parentElement.style.overflow = "hidden";
+itemsToAnimateFromTop.forEach((el) => {
+   el.style.transform = "translateY(-150%)";
+});
+itemsToAnimateFromBottom.forEach((el) => {
+   el.style.transform = "translateY(150%)";
+});
+itemsToAnimateFromLeft.forEach((el) => {
+   el.style.transform = "translateX(-120%)";
+});
+itemsToAnimateFromRight.forEach((el) => {
+   el.style.transform = "translateX(120%)";
+});
+itemsToAnimateScale.forEach((el) => {
+   el.style.transform = "scale(0.8)";
    el.style.opacity = "0";
-   el.style.transform = "translateY(-120%)";
+   el.style.transitionProperty = "transform, opacity";
+   el.style.transitionDuration = "1s, 2s";
+   el.style.transitionDelay = "0s, 0s";
+   el.style.transitionTimingFunction = "ease, ease";
+});
+
+itemsToAnimate.forEach((el) => {
+   // el.parentElement.style.overflow = "hidden";
+   el.style.opacity = "0";
+   // el.style.transform = "translateY(-120%)";
    el.style.transitionProperty = "transform, opacity";
    el.style.transitionDuration = "1s, 2s";
    el.style.transitionDelay = "0s, 0s";
@@ -32,6 +58,12 @@ function showAnimatedItems(e) {
    itemsToAnimate.forEach((el) => {
       if (el.getBoundingClientRect().top <= viewportHeight / 1.3) {
          el.style.transform = "translateX(0)";
+         el.style.opacity = "1";
+      }
+   });
+   itemsToAnimateScale.forEach((el) => {
+      if (el.getBoundingClientRect().top <= viewportHeight / 1.3) {
+         el.style.transform = "scale(1)";
          el.style.opacity = "1";
       }
    });
